2561cbdb8ef17292bd8f9d3764edbf5e74b5c252
NilsOveTen <github.nils.ove@tendenes.com>
2024-02-01 09:56:04 +0100

    refactor: rm rabbit & add functionality for saving 1 resource per graph

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM maven:3-openjdk-17-slim AS build
+FROM maven:3-eclipse-temurin-17-alpine AS build
 WORKDIR /app
 COPY pom.xml ./
 COPY src ./src
--- src/main/java/no/fdk/service/UpdateService.java
+++ src/main/java/no/fdk/service/UpdateService.java
@@ -2,84 +2,72 @@ package no.fdk.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import no.fdk.configuration.HarvestGraphNames;
-import no.fdk.configuration.HarvestURI;
+import no.fdk.configuration.HarvestGraphProperties;
 import no.fdk.model.fuseki.action.CatalogType;
 import org.apache.jena.query.ReadWrite;
 import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFuseki;
 import org.apache.jena.riot.Lang;
-import org.apache.jena.riot.RDFDataMgr;
 import org.springframework.stereotype.Service;
+
+import java.io.StringReader;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class UpdateService {
-    private final HarvestGraphNames graphNames;
-    private final HarvestURI uris;
+    private final HarvestGraphProperties graphProperties;
 
-    private RDFConnection harvestedConnection() {
+    private RDFConnection fusekiConnection() {
         return RDFConnectionFuseki.create()
-                .destination(uris.getFusekiHarvested())
+                .destination(graphProperties.getUri())
                 .build();
     }
 
-    public void updateForCatalogType(String catalogKey) {
-        CatalogType catalogType = catalogTypeFromKey(catalogKey);
-        if (catalogType != null) {
-            log.debug("updating the {} graph", catalogType.name());
-            try (RDFConnection conn = harvestedConnection() ) {
-                Model catalogModel = RDFDataMgr.loadModel(uriForCatalogType(catalogType), Lang.TURTLE);
-                String graphName = graphNameForCatalogType(catalogType);
+    private Model readTurtleBody(String body) {
+        Model m = ModelFactory.createDefaultModel();
 
-                conn.begin(ReadWrite.WRITE);
-                conn.put(graphName, catalogModel);
-            } catch (Exception exception) {
-                log.error("update of {} failed", catalogType.name(), exception);
-            }
+        StringReader reader = new StringReader(body);
+        m.read(reader, "", Lang.TURTLE.getName());
+
+        return m;
+    }
+
+    public void updateGraph(CatalogType type, String id, String body) {
+        try (RDFConnection conn = fusekiConnection() ) {
+            String graphName = graphName(type, id);
+            Model catalogModel = readTurtleBody(body);
+
+            conn.begin(ReadWrite.WRITE);
+            conn.put(graphName, catalogModel);
+        } catch (Exception exception) {
+            log.error("update for id {} with catalog type {} failed", id, type, exception);
         }
     }
 
-    private CatalogType catalogTypeFromKey(String catalogKey) {
-        CatalogType catalogType;
-        switch (catalogKey) {
-            case "concepts.reasoned" -> catalogType = CatalogType.CONCEPTS;
-            case "dataservices.reasoned" -> catalogType = CatalogType.DATA_SERVICES;
-            case "datasets.reasoned" -> catalogType = CatalogType.DATASETS;
-            case "events.reasoned" -> catalogType = CatalogType.EVENTS;
-            case "informationmodels.reasoned" -> catalogType = CatalogType.INFORMATION_MODELS;
-            case "public_services.reasoned" -> catalogType = CatalogType.PUBLIC_SERVICES;
-            default -> catalogType = null;
+    public void deleteGraph(CatalogType type, String id) {
+        try (RDFConnection conn = fusekiConnection() ) {
+            String graphName = graphName(type, id);
+
+            conn.begin(ReadWrite.WRITE);
+            conn.delete(graphName);
+        } catch (Exception exception) {
+            log.error("delete for id {} with catalog type {} failed", id, type, exception);
         }
-        return catalogType;
     }
 
-    private String uriForCatalogType(CatalogType catalogType) {
-        String uri;
-        switch (catalogType) {
-            case CONCEPTS -> uri = uris.getConcepts();
-            case DATA_SERVICES -> uri = uris.getDataservices();
-            case DATASETS -> uri = uris.getDatasets();
-            case EVENTS -> uri = uris.getEvents();
-            case INFORMATION_MODELS -> uri = uris.getInformationmodels();
-            case PUBLIC_SERVICES -> uri = uris.getPublicservices();
-            default -> uri = null;
-        }
-        return uri;
-    }
-
-    private String graphNameForCatalogType(CatalogType catalogType) {
+    private String graphName(CatalogType type, String id) throws Exception {
         String graphName;
-        switch (catalogType) {
-            case CONCEPTS -> graphName = graphNames.getConcepts();
-            case DATA_SERVICES -> graphName = graphNames.getDataservices();
-            case DATASETS -> graphName = graphNames.getDatasets();
-            case EVENTS -> graphName = graphNames.getEvents();
-            case INFORMATION_MODELS -> graphName = graphNames.getInformationmodels();
-            case PUBLIC_SERVICES -> graphName = graphNames.getPublicservices();
-            default -> graphName = null;
+        switch (type) {
+            case CONCEPTS-> graphName = graphProperties.getConcepts() + id;
+            case DATA_SERVICES -> graphName = graphProperties.getDataservices() + id;
+            case DATASETS -> graphName = graphProperties.getDatasets() + id;
+            case EVENTS -> graphName = graphProperties.getEvents() + id;
+            case INFORMATION_MODELS -> graphName = graphProperties.getInformationmodels() + id;
+            case SERVICES -> graphName = graphProperties.getServices() + id;
+            default -> throw new Exception("Unable to create graph name for type " + type + " and id " + id);
         }
         return graphName;
     }
