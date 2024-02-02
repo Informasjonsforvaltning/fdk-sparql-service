249632bcd83192e5ad422aa33399348315d05f27
NilsOveTen <github.nils.ove@tendenes.com>
2024-02-02 16:32:19 +0100

    feat: consume events for events, infomodels and services

--- src/main/java/no/fdk/sparqlservice/configuration/KafkaConsumerConfig.java
+++ src/main/java/no/fdk/sparqlservice/configuration/KafkaConsumerConfig.java
@@ -4,6 +4,9 @@ package no.fdk.sparqlservice.configuration;
 import no.fdk.concept.ConceptEvent;
 import no.fdk.dataservice.DataServiceEvent;
 import no.fdk.dataset.DatasetEvent;
+import no.fdk.event.EventEvent;
+import no.fdk.informationmodels.InformationModelEvent;
+import no.fdk.service.ServiceEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -42,6 +45,39 @@ public class KafkaConsumerConfig {
             ConsumerFactory<String, DatasetEvent> consumerFactory
     ) {
         ConcurrentKafkaListenerContainerFactory<String, DatasetEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        ContainerProperties props = factory.getContainerProperties();
+        props.setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, EventEvent> eventListenerContainerFactory(
+            ConsumerFactory<String, EventEvent> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, EventEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        ContainerProperties props = factory.getContainerProperties();
+        props.setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, InformationModelEvent> infoModelListenerContainerFactory(
+            ConsumerFactory<String, InformationModelEvent> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, InformationModelEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        ContainerProperties props = factory.getContainerProperties();
+        props.setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, ServiceEvent> serviceListenerContainerFactory(
+            ConsumerFactory<String, ServiceEvent> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, ServiceEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
         ContainerProperties props = factory.getContainerProperties();
         props.setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
